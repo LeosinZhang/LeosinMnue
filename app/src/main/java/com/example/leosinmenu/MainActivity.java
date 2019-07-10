b06396a890bf77c966f25d9d2f838a75a8ceb2aa
LeosinZhang <at.zhangxin@foxmail.com>
2019-07-10 11:28:15 +0800

    modify gradle & add apk file

--- app/src/main/java/com/example/leosinmenu/MainActivity.java
+++ app/src/main/java/com/example/leosinmenu/MainActivity.java
@@ -16,45 +16,36 @@ public class MainActivity extends Activity {
     private CircleMenuLayout mCircleMenuLayout;
 
     private String[] mItemTexts = new String[]{"零食 ", "水果", "面条",
-            "炒饭", "火锅", "牛排", "烧烤", "西北风", "中餐", "汉堡","小龙虾","干锅"};
+            "炒饭", "火锅", "牛排", "烧烤", "西北风", "中餐", "汉堡", "小龙虾", "干锅"};
     private int[] mItemImgs = new int[]{
             R.mipmap.snack, R.mipmap.fruit,
             R.mipmap.noodles, R.mipmap.friedrice,
             R.mipmap.hotpot, R.mipmap.steak,
             R.mipmap.bbq, R.mipmap.wind,
             R.mipmap.chinesefood, R.mipmap.hamburger,
-            R.mipmap.crayfish,R.mipmap.drypot,
+            R.mipmap.crayfish, R.mipmap.drypot,
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //自已切换布局文件看效果
         setContentView(R.layout.activity_main);
 
-        mCircleMenuLayout = (CircleMenuLayout) findViewById(R.id.id_menulayout);
+        mCircleMenuLayout = findViewById(R.id.id_menulayout);
         mCircleMenuLayout.setMenuItemIconsAndTexts(mItemImgs, mItemTexts);
-
-
         mCircleMenuLayout.setOnMenuItemClickListener(new CircleMenuLayout.OnMenuItemClickListener() {
-
             @Override
             public void itemClick(View view, int pos) {
                 Toast.makeText(MainActivity.this, mItemTexts[pos],
                         Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
             public void itemCenterClick(View view) {
                 Toast.makeText(MainActivity.this,
-                        "you can do something just like ccb  ",
+                        "今天吃什么呢 ^_^ ",
                         Toast.LENGTH_SHORT).show();
-
             }
         });
-
     }
-
 }
